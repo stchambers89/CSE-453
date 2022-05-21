01b53c3913126a4e69170cf092c9fde65e08f9be
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2022-05-21 17:00:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,86 @@
-def main():
+# Declare variables.
+file1 = ''
+file2 = ''
 
+# Get input from user.
+first_filename = input('Specify the first filename: ')
+second_filename = input('Specify the second filename: ')
 
-    class PathSegment:
-        def __init__(self, first, last, total, value):
-            self.first = first
-            self.last = last
-            self.total = total
-            self.value = value
-    
-    def getPaths():
-        filePath1 = input('Enter the frist filename: ')
-        filePath2 = input('Enter the second filename: ')
-        paths = [filePath1, filePath2]
-        return paths
+# Write a canonicalization function.
+def canon(filename):
 
-    def getPathEndings(pathList):
-        pathEndings = []
-        for path in pathList:
-            splitPath = path.split('\\')
-            pathEnd = splitPath[-1]
-            pathEndings.append(pathEnd.lower())
+    # Declare temporary string variable
+    temp_string = ''
+
+    for i in range(len(filename)):
+        if filename[i] == "/" or filename[i] == "." or filename[i] == "\\" or filename[i] == ":":
+            i += 1
+        else:
+            temp_string += filename[i]
+
+    # Convert to lowercase string.
+    temp_string = temp_string.lower()
+
+    # For Testing
+    # print(temp_string)
+
+    return temp_string
+
+# Write a homograph function.
+def  homograph(first_filename, second_filename):
+
+    # Declare values
+    isHomograph = False
+    count = 0
+
+    # Call canon()
+    f1 = canon(first_filename)
+    f2 = canon(second_filename)
+
+    #Reverse the strings for comparison.
+    file1 = f1[::-1]
+    file2 = f2[::-1]
+
+    # For Testing.
+    # print(f'File1: {file1}')
+    # print(f'File2: {file2}')
+
+    # Compare files
+    if len(file1) < len(file2):
+
+        # Use for loop to compare strings.
+        for i in range(len(file1)):
+
+            # Compare each letter to each other. If they are not
+            # identical then increase the count variable. If count
+            # ends up greater than or equal to 1, it must be a non-
+            # homograph.
+            if file1[i] != file2[i]:
+                count += 1
+            else:
+                i += 1
+
+        # If count == 0, update the boolean value.
+        if count == 0:
+            isHomograph = True
+
+    # Else file2 is smaller and we should use that one.         
+    else: 
         
-   
-    
-   
+        for i in range(len(file2)):
 
-    pathList = getPaths()
-    getPathEndings(pathList)
+            if file2[i] != file1[i]:
+                count += 1
+            else:
+                i += 1
 
+        if count == 0:
+            isHomograph = True
 
+    # Print appropriate statement.
+    if isHomograph == True:
+        print("The paths are homographs")
+    else:
+        print("The paths are not homographs")
 
-
-
-    
-    #fileList1 = splitFile(filePath1)
-    #fileList2 = splitFile(filePath2)
-
-    #filePathEnd1 = fileList1[-1]
-    #filePathEnd2 = fileList2[-1]
-
-
-    # def compareFileNames(firstFile, secondFile):
-    #     return firstFile == secondFile
-
-    #def canon(file, splitFileList):
-        #convert string into ascii values (h = 104, e = 101, etc)
-        #for letter
-        #value = [ord(character) for character in splitFileList]
-        #print(value)
-    
-    #canon(firstFile)
-    
-    # Determines if two file paths are the same.
-    #def homograph():
-       #print('This function exists and works in some capacity. 50%!')
-        
-if __name__ == "__main__":
-    main()
-
-
-
-     #print(' ') 
-    #print("the first filename is: " + firstFile)
-    ##print("the second filename is: " + forbiddenFilePath)
-    #if secondFile == forbiddenFilePath:
-    #    print("True")
-    
-
-
-    
-    
-#TO DO
-# Find Symbols for windows shell navigation
-# Create non-homographs
-# Compare two filepaths to see if they lead to the same file 
-# (our homograph function should rely on canon function)
-# 
-
-
-# Non Homograph Test Cases
-# topSecret.txt
-# ..\topSecret.txt
-# ..\Forbidden\topSecret.txt
-# ..\Jim\Forbidden\topSecret.txt
-
-# Homograph Test Cases
-# C:\User\Jim\Forbidden\topSecret.txt
-# C:.\..\Jim\Forbidden\topSecret.txt
-# C:.\..\..\Forbidden\topSecret.txt
-# C:.\..\..\..\topSecret.txt
-
-# WHYYYYYY?!?!?!?!?!?!?!?!
+homograph(first_filename, second_filename)
